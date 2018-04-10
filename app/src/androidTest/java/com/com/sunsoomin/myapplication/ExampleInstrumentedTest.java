32b1eecbca90744bb7d02992fac8b5833fc512e1
na5ming <38244708+na5ming@users.noreply.github.com>
2018-04-10 19:00:44 +0900

    내마음 출력함
    내 솔직한 마음을 출력함

--- app/src/androidTest/java/com/com/sunsoomin/myapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/com/sunsoomin/myapplication/ExampleInstrumentedTest.java
@@ -22,5 +22,6 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.com.sunsoomin.myapplication", appContext.getPackageName());
+        System.out.println("I love SOOMIN!!!!!!!!!!!!!!!!!!!!!!!");
     }
 }
